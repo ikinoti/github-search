0623b35ddf3fabfd40b5e1ea134bf3ed565e7efa
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-28 23:09:47 +0300

    import all the moddules needed to run the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+import { GithubFormComponent } from './components/github-form/github-form.component';
+import { GithubReposComponent } from './components/github-repos/github-repos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, GithubProfileComponent, GithubFormComponent, GithubReposComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
