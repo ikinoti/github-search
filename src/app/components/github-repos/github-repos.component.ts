c6dedb3064c62c47cf006fb4123a5a527282b806
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-28 22:54:20 +0300

    define input property binding to pass data from parent component to child

--- src/app/components/github-repos/github-repos.component.ts
+++ src/app/components/github-repos/github-repos.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-github-repos',
   templateUrl: './github-repos.component.html',
-  styleUrls: ['./github-repos.component.css']
+  styleUrls: ['./github-repos.component.css'],
 })
 export class GithubReposComponent implements OnInit {
+  @Input() githubRepos: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
