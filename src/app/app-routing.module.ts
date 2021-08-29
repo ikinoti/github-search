e43f3b7dabe8c682aec37cc3f9004559d7f70ec2
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-29 20:23:47 +0300

    define route paths for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubFormComponent } from './components/github-form/github-form.component';
 import { GithubProfileComponent } from './components/github-profile/github-profile.component';
 import { GithubReposComponent } from './components/github-repos/github-repos.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
+  { path: 'search', component: GithubFormComponent },
   { path: 'profile', component: GithubProfileComponent },
   { path: 'repos', component: GithubReposComponent },
+  { path: '**', component: NotFoundComponent },
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
 ];
 
 @NgModule({
