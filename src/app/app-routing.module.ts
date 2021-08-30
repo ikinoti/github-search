6dc3cf6cb50674e6bf622693b401e4e8f585dd23
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-30 21:01:19 +0300

    modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GithubFormComponent } from './components/github-form/github-form.component';
-import { GithubProfileComponent } from './components/github-profile/github-profile.component';
-import { GithubReposComponent } from './components/github-repos/github-repos.component';
+// import { GithubFormComponent } from './components/github-form/github-form.component';
+// import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+// import { GithubReposComponent } from './components/github-repos/github-repos.component';
 
 const routes: Routes = [
-  { path: 'search', component: GithubFormComponent },
-  { path: 'profile', component: GithubProfileComponent },
-  { path: 'repos', component: GithubReposComponent },
-  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  //   { path: 'search', component: GithubFormComponent },
+  //   { path: 'profile', component: GithubProfileComponent },
+  //   { path: 'repos', component: GithubReposComponent },
+  //   { path: '', redirectTo: '/search', pathMatch: 'full' },
 ];
 
 @NgModule({
