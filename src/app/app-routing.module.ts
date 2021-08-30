de8df39410245bed4064ceecc88f395633d242bc
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-30 07:36:01 +0300

    MOdified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { GithubFormComponent } from './components/github-form/github-form.component';
 import { GithubProfileComponent } from './components/github-profile/github-profile.component';
 import { GithubReposComponent } from './components/github-repos/github-repos.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'search', component: GithubFormComponent },
   { path: 'profile', component: GithubProfileComponent },
   { path: 'repos', component: GithubReposComponent },
-  { path: '**', component: NotFoundComponent },
   { path: '', redirectTo: '/search', pathMatch: 'full' },
 ];
 
